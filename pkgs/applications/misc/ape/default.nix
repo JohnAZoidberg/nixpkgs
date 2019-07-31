1a7c0251349bf593bf8b94389bc1979d202d35a7
Daniel Schaefer <git@danielschaefer.me>
2019-07-31 18:44:41 +0200

    ape: 6.7-131003 -> 6.7.180715
    Finally fixes the build after 3 months.

--- pkgs/applications/misc/ape/default.nix
+++ pkgs/applications/misc/ape/default.nix
@@ -1,14 +1,7 @@
-{ stdenv, swiProlog, makeWrapper,
-  fetchFromGitHub,
-  lexicon ? "lexicon/clex_lexicon.pl",
-  pname ? "ape",
-  description ? "Parser for Attempto Controlled English (ACE)",
-  license ? with stdenv.lib; licenses.lgpl3
-  }:
-
+{ stdenv, swiProlog, makeWrapper, fetchFromGitHub }:
 stdenv.mkDerivation rec {
-  name = "${pname}-${version}";
-  version = "6.7-131003";
+  pname = "ape";
+  version = "6.7.180715";
 
   buildInputs = [ swiProlog makeWrapper ];
 
@@ -16,29 +9,22 @@ stdenv.mkDerivation rec {
      owner = "Attempto";
      repo = "APE";
      rev = version;
-     sha256 = "0cw47qjg4896kw3vps6rfs02asvscsqvcfdiwgfmqb3hvykb1sdx";
+     sha256 = "1jnr6y4kc6d5rjy6bbbnn4n7rl6ajpvw4xf4067wjh28c9scjwg3";
   };
 
-  patchPhase = ''
-    # We move the file first to avoid "same file" error in the default case
-    cp ${lexicon} new_lexicon.pl
-    rm lexicon/clex_lexicon.pl
-    cp new_lexicon.pl lexicon/clex_lexicon.pl
+  patches = [ ./fix-build-system.patch ];
+
+  postInstall = ''
+    install -Dm755 ape.exe $out/bin/ape
+    wrapProgram $out/bin/ape --add-flags ace
   '';
 
-  buildPhase = ''
-    make build
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp ape.exe $out
-    makeWrapper $out/ape.exe $out/bin/ape --add-flags ace
-  '';
+  doCheck = true;
 
   meta = with stdenv.lib; {
-    description = description;
-    license = license;
+    description = "Parser for Attempto Controlled English (ACE)";
+    homepage = "https://github.com/Attempto/APE";
+    license = licenses.lgpl3;
     platforms = platforms.unix;
     maintainers = with maintainers; [ yrashk ];
   };
